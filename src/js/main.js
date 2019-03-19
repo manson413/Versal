da861ba6fd433eadba871112686b397c9c01bfe5
front-end-p <front.end.p.dzencode@gmail.com>
2019-03-19 17:40:58 +0200

    finish Home page

--- src/js/main.js
+++ src/js/main.js
@@ -14,4 +14,14 @@ window.onload = function(){
         }
     });
 
+    //first screen scroll
+    $('#firstScreenScroll').click(function() {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 800);
+        return false;
+    });
+
 };
